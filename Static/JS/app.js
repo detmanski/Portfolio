9504dd0aca46b5b2931ed90e51545d9c4073c03a
detmanski <etmanskid@gmail.com>
2023-07-05 08:50:57 -0400

    updates to code

---
+++ Static/JS/app.js
@@ -0,0 +1,13 @@
+  document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+      autoplay: {
+        delay: 5000,
+      },
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
+
